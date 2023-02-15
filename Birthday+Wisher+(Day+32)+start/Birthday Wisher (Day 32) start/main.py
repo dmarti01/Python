29f92de3de8bdc34020849e98b6b39df996a9e85
dmarti01 <73558384+dmarti01@users.noreply.github.com>
2023-02-15 22:23:39 +0100

    Add files via upload

---
+++ Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "someemail@gmail.com"
+MY_PASSWORD = "password"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 3:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Thursday Motivation\n\n{quote}"
+        )
